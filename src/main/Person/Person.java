16cf086c044e5ba0be30ebdfe90ec24e85a5603e
Wei En Zheng <99640676+WeiEZheng@users.noreply.github.com>
2022-04-06 11:40:03 -0400

    minor fix

--- src/main/Person/Person.java
+++ src/main/Person/Person.java
@@ -1,4 +1,28 @@
 package Person;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class Person {
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+
+    public int getAge() {
+        Period period = Period.between(birthday, LocalDate.now());
+        return period.getYears();
+    }
+
+    public void printPerson() {
+        String print = "Name: "+name+
+                "\nBirthdate: "+birthday.toString()+
+                "\nGender: "+gender+
+                "\nEmail Adddress: "+emailAddress;
+        System.out.println(print);
+    }
 }
